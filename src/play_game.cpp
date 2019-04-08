e4eea1b672de1d39c462927a98c77a8fa3831451
Prabin Rath <prabinrath123@gmail.com>
2019-04-08 20:39:43 +0530

    clean file added

--- src/play_game.cpp
+++ src/play_game.cpp
@@ -1,4 +1,3 @@
-
 #include<game.h>
 #include <iostream>
 #include <fstream>
@@ -7,6 +6,8 @@ int main(int argc, char** argv)
 {
 	TimePoint time1=Clock::now();
 	BalanceGame game(argc,argv);
+	ofstream file;
+  	file.open ("src/dataset.txt", ios::app);
 	while(true)
 	{
 		game.draw();
@@ -19,18 +20,12 @@ int main(int argc, char** argv)
 		{
 			break;
 		}
-		// file saving
-		ofstream file;
-  		file.open ("src/dataset.txt", ios::app);
-		char buffer[200];
-		
-		sprintf(buffer,"%f,%f,%f,%f,%f\n",game.ballPos.length(),game.ballVel.length(),game.curang,game.motionang,game.getTAR());
-		//cout<<game.ballPos.length()<<" "<<game.ballVel.length()<<" "<<game.curang<<" "<<game.motionang<<" "<<game.getTAR()<<endl;
-		//cout<<buffer<<endl;
-		//float update_time=time(0);
-		
+				
 		if(getTimeDifference(Clock::now(), time1)>100 )
 		{
+			// file saving
+			char buffer[200];
+			sprintf(buffer,"%f,%f,%f,%f,%f\n",game.ballPos.length(),game.ballVel.length(),game.curang,game.motionang,game.getTAR());
 			if(game.ballPos.getX()>=-20 && game.ballPos.getX()<=20 && game.ballPos.getY()>=-20 && game.getTAR()!=0)
 				file << buffer;
 			time1=Clock::now();
